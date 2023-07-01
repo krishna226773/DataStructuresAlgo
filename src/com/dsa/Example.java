0689aab7638f2e3f22bfae0b2ff3246a3b79a56c
krishna <krishnaprasad.dasari123@gmail.com>
2023-07-01 09:44:15 +0530

    First commit

---
+++ src/com/dsa/Example.java
@@ -0,0 +1,12 @@
+package com.dsa;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		System.out.println("EXMPALE PROJECT");
+		// TODO Auto-generated method stub
+
+	}
+
+}
